7faaf0c23131f2020fcb15e0c838b0c714901ee0
Zac Smith <zrs3141592@gmail.com>
2020-08-24 20:29:00 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const dotenv = require('dotenv')
+
+// load config
+dotenv.config({ path: './config/config.env' })
+
+const app = express()
+
+const PORT = process.env.PORT || 5555
+app.listen(PORT, () => {
+  console.log(
+    `\n** Server running in ${process.env.NODE_ENV} mode on port ${PORT} **\n`
+  )
+})
