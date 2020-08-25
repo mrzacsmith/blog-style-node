4fc4da556f4c4d23ad9145bf1858d1d0d009a8c2
Zac Smith <zrs3141592@gmail.com>
2020-08-24 20:48:48 -0600

    add middleware

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const morgan = require('morgan')
+const helmet = require('helmet')
+const exphbs = require('express-handlebars')
 const connectDB = require('./config/db.js')
 
 // load config
@@ -8,6 +11,16 @@ dotenv.config({ path: './config/config.env' })
 connectDB()
 
 const app = express()
+app.use(helmet())
+
+// logging for dev only
+if (process.env.NODE_DEV == 'development') {
+  app.use(morgan('dev'))
+}
+
+// handlebars
+app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', '.hbs')
 
 const PORT = process.env.PORT || 5555
 app.listen(PORT, () => {
